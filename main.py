67ef7145883729e67a6d8fe43865d061b70615ce
Chee Meng <CKWOK005@e.ntu.edu.sg>
2023-10-06 22:14:01 +0800

    remove plot and refactored KNN

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import sklearn 
 import scipy.stats as stats
+import time
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, mean_squared_error
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import GridSearchCV
-import time
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_score, cross_val_predict
 
+
+# Pre-processing data
 startTimer = time.time()
+kvalue = 0
 # Referenced from adult.names
 columns = ["age", "workclass", "fnlwgt", "education", "education-num","marital-status", "occupation", "relationship",
           "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "income"]
@@ -26,12 +29,13 @@ train = train.dropna(axis=0)
 test[test=='?']=np.nan
 test = test.dropna(axis=0)
 cat_attributes = train.select_dtypes(include=['object'])
-cat_attributes.describe()
+# cat_attributes.describe()
 num_attributes = train.select_dtypes(include=['int'])
-num_attributes.describe()
+# num_attributes.describe()
 train["income"]=train["income"].map({"<=50K":0,">50K":1})
 test["income"]=test["income"].map({"<=50K.":0,">50K.":1})
 
+# Examining Biserial correlation
 a=['age','fnlwgt','education-num','capital-gain','capital-loss','hours-per-week','income']
 for i in a:
     print(i,':',stats.pointbiserialr(train['income'],train[i])[0])
@@ -69,6 +73,10 @@ test_y=test["income"]
 del train_X["income"]
 del test_X["income"]
 
+print('\n----------- STARTING DATA PRE-PROCESSING -----------')
+print('Time taken to pre-process data', time.time() - startTimer)
+print('----------- END OF DATA PRE-PROCESSING -----------')
+
 def plot(column):
     if train[column].dtype != 'int64':
         f, axes = plt.subplots(1,1,figsize=(15,5))
@@ -82,36 +90,12 @@ def plot(column):
         plt.show()
     plt.show()
 
-def plotKnn():
-    error=[]
-    for i in range(15,35,1):
-        knn_model = KNeighborsClassifier(n_neighbors=i)
-        knn_model.fit(train_X,train_y)
-        knn_pred = knn_model.predict(test_X)
-        error.append(np.mean(knn_pred != test_y))
-
-        print('\nk = ', i )
-        print('Train Accuracy Score = ', round(knn_model.score(train_X, train_y) * 100, 4),"%")
-        print("Test Accuracy Score:", round(accuracy_score(test_y, knn_pred) * 100, 4),"%")
-        print("F1 Score: ", round(f1_score(test_y, knn_pred) * 100,4),"%")
-        print("MSE: ", round(mean_squared_error(test_y, knn_pred) * 100,4),"%")
-        
-        print(confusion_matrix(test_y,knn_pred))
-        print(classification_report(test_y,knn_pred))
-
-    plt.figure(figsize=(12, 6))
-    plt.plot(range(15,35,1), error, color='red', linestyle='dashed', marker='x',markerfacecolor='blue', markersize=10)
-    plt.title('Error Rate K Value')
-    plt.xlabel('K Value')
-    plt.ylabel('Mean Squared Error')
-    plt.show()
-
 def predictKnn():
+    print('\n----------- FIND BEST KNN COMPUTATION -----------')
     startTimer = time.time()
-    kvalue = 0
     compareValue = 100
     error=[]
-    for i in range(15,35,1):
+    for i in range(15,36,1):
         knn_model = KNeighborsClassifier(n_neighbors=i)
         knn_model.fit(train_X,train_y)
         knn_pred = knn_model.predict(test_X)
@@ -119,11 +103,22 @@ def predictKnn():
         if(round(mean_squared_error(test_y, knn_pred) * 100,4)<compareValue): 
             compareValue=round(mean_squared_error(test_y, knn_pred) * 100,4) 
             kvalue = i
+    stopTimer = time.time()
+    knn_model = KNeighborsClassifier(n_neighbors=kvalue)
+    knn_model.fit(train_X,train_y)
+    knn_pred = knn_model.predict(test_X)
+    print('Train Accuracy Score = ', round(knn_model.score(train_X, train_y) * 100, 2),"%")
+    print("Test Accuracy Score:", round(accuracy_score(test_y, knn_pred) * 100, 2),"%")
+    print("F1 Score: ", round(f1_score(test_y, knn_pred) * 100,2),"%")
+    print("MSE: ", round(mean_squared_error(test_y, knn_pred) * 100,2),"%")
+    print(confusion_matrix(test_y,knn_pred))
+    print(classification_report(test_y,knn_pred))
+    print('best kvalue =', kvalue,'| compute time: ',stopTimer-startTimer)
+    print('\n----------- START KNN COMPUTATION (Euclidean & Manhattan) -----------')
+    startTimer = time.time()
     metric_accuracy_training = {}
     metric_accuracy_testing = {}
-
     dist_calc = ['euclidean', 'manhattan']
-
     for i in dist_calc:
         model = KNeighborsClassifier(kvalue, metric = i) 
         model.fit(train_X, train_y)
@@ -131,9 +126,11 @@ def predictKnn():
         metric_accuracy_testing[i] = model.score(test_X, test_y) 
         stopTimer = time.time()
         print('\nCalculation : ', i)
-        print('Training accuracy :  ', knn_model.score(train_X, train_y))
-        print('Testing accuracy : ', knn_model.score(test_X, test_y))
-        print('Computational time :', time.time() - startTimer)
+        print('Training Accuracy :  ', round(knn_model.score(train_X, train_y)*100, 2),'%')
+        print('Testing Accuracy : ', round(knn_model.score(test_X, test_y)*100, 2),'%')
+        print('Computational Time :', stopTimer - startTimer)
+    print('\nTotal Compute Time:', time.time()-startTimer)
+    print('----------- END KNN COMPUTATION -----------')
+
 
 predictKnn()
-
